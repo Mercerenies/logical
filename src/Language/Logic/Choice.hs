9cc2fb723b2bae1f67321624687893f8824965de
Silvio Mayolo <mercerenies@comcast.net>
2020-06-16 16:12:54 -0400

    Once semantics

--- src/Language/Logic/Choice.hs
+++ src/Language/Logic/Choice.hs
@@ -10,6 +10,7 @@ import qualified Control.Applicative as App
 data Choice m a where
     Empty :: Choice m a
     Choose :: m a -> m a -> Choice m a
+    Once :: m a -> Choice m a
 
 makeSem ''Choice
 
@@ -23,8 +24,9 @@ nonDetToChoice = interpretH $ \case
                        b'' = raise $ nonDetToChoice b'
                    choose a'' b''
 
-runChoice :: forall f r a. Alternative f => Sem (Choice ': r) a -> Sem r (f a)
-runChoice = ND.runNonDet . reinterpretH go
+runChoice0 :: forall f r a. Alternative f =>
+              (forall r1. Sem (ND.NonDet ': r1) a -> Sem r1 (f a)) -> Sem (Choice ': r) a -> Sem r (f a)
+runChoice0 f = f . reinterpretH go
     where go :: forall m x f1. Choice m x -> Sem (WithTactics Choice f1 m (ND.NonDet : r)) (f1 x)
           go Empty = App.empty
           go (Choose a b) = do
@@ -33,3 +35,15 @@ runChoice = ND.runNonDet . reinterpretH go
             let a'' = raise $ interpretH go a'
                 b'' = raise $ interpretH go b'
             a'' <|> b''
+          go (Once a) = do
+            a' <- runT a
+            a'' <- raise $ runChoiceMaybe a'
+            case a'' of
+              Nothing -> App.empty
+              Just x -> pure x
+
+runChoice :: Alternative f => Sem (Choice ': r) a -> Sem r (f a)
+runChoice = runChoice0 ND.runNonDet
+
+runChoiceMaybe :: Sem (Choice ': r) a -> Sem r (Maybe a)
+runChoiceMaybe = runChoice0 ND.runNonDetMaybe
