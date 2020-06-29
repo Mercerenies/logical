218377c0a549c3f80adee9284d01cd549520998f
Silvio Mayolo <mercerenies@comcast.net>
2020-06-29 14:42:19 -0400

    Fixed Once in the choice implementation

--- src/Language/Logic/Choice.hs
+++ src/Language/Logic/Choice.hs
@@ -1,16 +1,23 @@
 
-module Language.Logic.Choice where
+module Language.Logic.Choice(Choice(..), empty, choose, once,
+                             nonDetToChoice, runChoice0, runChoice, runChoiceMaybe, runChoiceOnce,
+                             runNonDetOnce) where
 
 import Polysemy
+import qualified Polysemy.Error as Err
 import qualified Polysemy.NonDet as ND
 
 import Control.Applicative(Alternative((<|>)))
 import qualified Control.Applicative as App
+import Control.Arrow((>>>))
 
 data Choice m a where
     Empty :: Choice m a
     Choose :: m a -> m a -> Choice m a
     Once :: m a -> Choice m a
+
+-- Error type used to abort the computation after one success.
+data PrivateShortCircuit a = PrivateShortCircuit { unPrivateShortCircuit :: a }
 
 makeSem ''Choice
 
@@ -37,7 +44,7 @@ runChoice0 f = f . reinterpretH go
               raise $ interpretH go b'
           go (Once a) = do
             a' <- runT a
-            a'' <- raise $ runChoiceMaybe a'
+            a'' <- raise $ runChoiceOnce a'
             case a'' of
               Nothing -> App.empty
               Just x -> pure x
@@ -47,3 +54,22 @@ runChoice = runChoice0 ND.runNonDet
 
 runChoiceMaybe :: Sem (Choice ': r) a -> Sem r (Maybe a)
 runChoiceMaybe = runChoice0 ND.runNonDetMaybe
+
+runChoiceOnce :: Sem (Choice ': r) a -> Sem r (Maybe a)
+runChoiceOnce = runChoice0 runNonDetOnce
+
+-- TODO If this is a performance problem (which it may very well end
+-- up being), provide a version that runs in IO.
+runNonDetOnce :: forall r a. Sem (ND.NonDet ': r) a -> Sem r (Maybe a)
+runNonDetOnce =
+    raiseUnder >>>
+    (>>= Err.throw . PrivateShortCircuit) >>>
+    ND.runNonDet @Maybe >>>
+    Err.runError @(PrivateShortCircuit a) >>>
+    fmap (either (Just . unPrivateShortCircuit) id)
+
+-- TODO If we're feeling particularly ambitious, we could consider
+-- trying to run the whole thing in one interpreter (rather than
+-- giving each Once its own interpreter) and make PrivateShortCircuit
+-- more sophisticated so it can catch the right exceptions and ignore
+-- others.
